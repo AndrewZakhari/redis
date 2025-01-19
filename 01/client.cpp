915feba5b8c4209e1ab484d57ccadb0bfddef32f
AndrewZakhari <andrewzakhari5@gmail.com>
2025-01-19 09:11:08 +0200

    07

--- 01/client.cpp
+++ 01/client.cpp
@@ -1,27 +1,28 @@
-#include <stdint.h>
-#include <stdio.h>
-#include <string>
 #include <assert.h>
+#include <stdint.h>
+#include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+#include <errno.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#include <errno.h>
-#include <stdlib.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <string>
 #include <vector>
 
 
-void die (const char *message){
-    int err = errno;
-    fprintf(stderr, "[%d] %s \n", err, message);
-    abort();
-}
-static void msg(const char *message) {
-    fprintf(stderr, "%s \n", message);
+static void msg(const char *msg) {
+    fprintf(stderr, "%s\n", msg);
 }
 
-static int32_t read_full(int fd, uint8_t *buf, size_t n) {
+static void die(const char *msg) {
+    int err = errno;
+    fprintf(stderr, "[%d] %s\n", err, msg);
+    abort();
+}
+
+static int32_t read_full(int fd, char *buf, size_t n) {
     while (n > 0) {
         ssize_t rv = read(fd, buf, n);
         if (rv <= 0) {
@@ -34,7 +35,7 @@ static int32_t read_full(int fd, uint8_t *buf, size_t n) {
     return 0;
 }
 
-static int32_t write_all(int fd, const uint8_t *buf, size_t n) {
+static int32_t write_all(int fd, const char *buf, size_t n) {
     while (n > 0) {
         ssize_t rv = write(fd, buf, n);
         if (rv <= 0) {
@@ -47,30 +48,38 @@ static int32_t write_all(int fd, const uint8_t *buf, size_t n) {
     return 0;
 }
 
-static void buf_append(std::vector<uint8_t> &buf, const uint8_t *data, size_t len) {
-    buf.insert(buf.end(), data, data + len);
-}
+const size_t k_max_msg = 4096;
 
-
-const size_t k_max_msg = 32 << 20;
-
-static int32_t send_req(int fd, const uint8_t *text, size_t len) {
+static int32_t send_req(int fd, const std::vector<std::string> &cmd) {
+    uint32_t len = 4;
+    for (const std::string &s : cmd) {
+        len += 4 + s.size();
+    }
     if (len > k_max_msg) {
         return -1;
     }
 
-    std::vector<uint8_t> wbuf;
-    buf_append(wbuf, (const uint8_t *)&len, 4);
-    buf_append(wbuf, text, len);
-    return write_all(fd, wbuf.data(), wbuf.size());
+    char wbuf[4 + k_max_msg];
+    memcpy(&wbuf[0], &len, 4);  // assume little endian
+    uint32_t n = cmd.size();
+    memcpy(&wbuf[4], &n, 4);
+    size_t cur = 8;
+    for (const std::string &s : cmd) {
+        uint32_t p = (uint32_t)s.size();
+        memcpy(&wbuf[cur], &p, 4);
+        memcpy(&wbuf[cur + 4], s.data(), s.size());
+        cur += 4 + s.size();
+    }
+    return write_all(fd, wbuf, 4 + len);
 }
-static int32_t read_res(int fd){
-    std::vector<uint8_t> rbuf;
-    rbuf.resize(4);
+
+static int32_t read_res(int fd) {
+    // 4 bytes header
+    char rbuf[4 + k_max_msg + 1];
     errno = 0;
-    int32_t err = read_full(fd, &rbuf[0], 4);
-    if(err){
-        if (errno == 0){
+    int32_t err = read_full(fd, rbuf, 4);
+    if (err) {
+        if (errno == 0) {
             msg("EOF");
         } else {
             msg("read() error");
@@ -79,23 +88,32 @@ static int32_t read_res(int fd){
     }
 
     uint32_t len = 0;
-    memcpy(&len, rbuf.data(), 4);
-    if(len > k_max_msg){
-        msg("too long");
+    memcpy(&len, rbuf, 4);  // assume little endian
+    if (len > k_max_msg) {
+        msg("too long");;
         return -1;
     }
 
-    rbuf.resize(4+len);
+    // reply body
     err = read_full(fd, &rbuf[4], len);
-    if(err) {
+    if (err) {
         msg("read() error");
         return err;
     }
-    printf("len: %u data:%.*s \n", len, len<100? len: 100, &rbuf[4]);
+
+    // print the result
+    uint32_t rescode = 0;
+    if (len < 4) {
+        msg("bad response");
+        return -1;
+    }
+    memcpy(&rescode, &rbuf[4], 4);
+    printf("server says: [%u] %.*s\n", rescode, len - 4, &rbuf[8]);
     return 0;
 }
-int main() { 
- int fd = socket(AF_INET, SOCK_STREAM, 0);
+
+int main(int argc, char **argv) {
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
     if (fd < 0) {
         die("socket()");
     }
@@ -109,33 +127,19 @@ int main() {
         die("connect");
     }
 
-   /* char msg[] = "hello";
-    write(fd, msg, strlen(msg));
+    std::vector<std::string> cmd;
+    for (int i = 1; i < argc; ++i) {
+        cmd.push_back(argv[i]);
+    }
+    int32_t err = send_req(fd, cmd);
+    if (err) {
+        goto L_DONE;
+    }
+    err = read_res(fd);
+    if (err) {
+        goto L_DONE;
+    }
 
-    char rbuf[64] = {};
-    ssize_t n = read(fd, rbuf, sizeof(rbuf) - 1);
-    if (n < 0) {
-        die("read");
-    }
-    printf("server says: %s\n", rbuf); */
-
-    std::vector<std::string> query_list = {
-        "hello1", "hello2", "hello3",
-        std::string(k_max_msg, 'z'),
-        "hello5"
-    };
-    for (const std::string &s : query_list){
-        int32_t err = send_req(fd, (uint8_t *)s.data(), s.size());
-        if(err){
-            goto L_DONE;
-        }
-    }
-    for (size_t i =0; i < query_list.size(); ++i){
-        int32_t err = read_res(fd);
-        if(err){
-            goto L_DONE;
-        }
-    }
 L_DONE:
     close(fd);
     return 0;
--- 01/server.cpp
+++ 01/server.cpp
@@ -1,5 +1,6 @@
 #include <assert.h>
 #include <cerrno>
+#include <map>
 #include <netinet/in.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -8,6 +9,7 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <poll.h>
+#include <string>
 #include <sys/poll.h>
 #include <unistd.h>
 #include <arpa/inet.h>
@@ -96,6 +98,91 @@ static Conn *handle_accept(int fd) {
     return conn;
 }
 
+static bool read_u32(const uint8_t *&cur, const uint8_t *end, uint32_t &out){
+    if(cur + 4 > end) {
+        return false;
+    }
+    memcpy(&out, cur, 4);
+    cur += 4;
+    return true;
+}
+
+static bool read_str(const uint8_t *&cur, const uint8_t *end, size_t n, std::string &out){
+    if(cur + n > end){
+        return false;
+    }
+    out.assign(cur, cur + n);
+    cur += n;
+    return true;
+}
+
+// +------+-----+------+-----+------+-----+-----+------+
+// | nstr | len | str1 | len | str2 | ... | len | strn |
+// +------+-----+------+-----+------+-----+-----+------+
+
+static int32_t parse_req(const uint8_t *data, size_t size, std::vector<std::string> &out){
+    const uint8_t *end = data + size;
+    uint32_t nstr = 0;
+    if (!read_u32(data, end, nstr)){
+        return -1;
+    }
+    if(nstr > k_max_msg){
+        return -1;
+    }
+    while (out.size() < nstr){
+        uint32_t len = 0;
+        if (!read_u32(data, end ,len)){
+            return -1;
+        }
+        out.push_back(std::string());
+        if(!read_str(data, end, len, out.back())){
+            return -1;
+        }
+    }
+    if (data != end){
+        return -1;
+    }
+    return 0;
+}
+
+enum {
+    RES_OK = 0,
+    RES_ERR = 1,
+    RES_NX = 2,
+};
+
+struct Response {
+    uint32_t status = 0;
+    std::vector<uint8_t> data;
+};
+
+static std::map<std::string, std::string> g_data;
+
+static void do_request(std::vector<std::string> &cmd, Response &out){
+    if(cmd.size() == 2 && cmd[0] == "get"){
+        auto it = g_data.find(cmd[1]);
+        if(it == g_data.end()){
+            out.status = RES_NX;
+            return;
+        }
+        const std::string &val = it->second;
+        out.data.assign(val.begin(), val.end());
+    } else if (cmd.size() == 3 && cmd[0] == "set"){
+        g_data[cmd[1]].swap(cmd[2]);
+    } else if (cmd.size() == 2 && cmd[0] == "del"){
+        g_data.erase(cmd[1]);
+    } else {
+        out.status = RES_ERR;
+    }
+}
+
+static void make_response(const Response &resp, std::vector<uint8_t> &out) {
+    uint32_t resp_len = 4 + (uint32_t)resp.data.size();
+    buf_append(out, (const uint8_t *)&resp_len, 4);
+    buf_append(out, (const uint8_t *)&resp.status, 4);
+    buf_append(out, resp.data.data(), resp.data.size());
+}
+
 static bool try_one_request(Conn *conn) {
     if (conn->incoming.size() < 4){
         return false;
@@ -112,10 +199,20 @@ static bool try_one_request(Conn *conn) {
     }
     const uint8_t *request = &conn->incoming[4];
 
-    printf("client says: len:%d data:%.*s\n", len, len < 100 ? len : 100, request);
+    // printf("client says: len:%d data:%.*s\n", len, len < 100 ? len : 100, request);
 
-    buf_append(conn->outgoing, (const uint8_t *)&len, 4);
-    buf_append(conn->outgoing, request, len);
+    // buf_append(conn->outgoing, (const uint8_t *)&len, 4);
+    // buf_append(conn->outgoing, request, len);
+
+    std::vector<std::string> cmd;
+    if(parse_req(request, len, cmd) < 0){
+        msg("bad request");
+        conn->want_close = true;
+        return false;
+    }
+    Response resp;
+    do_request(cmd, resp);
+    make_response(resp, conn->outgoing);
 
     buf_consume(conn->incoming, 4 + len);
 
